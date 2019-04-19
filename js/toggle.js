c9b2b439693e54813edb671604efb6940e6b6fa8
Mathew W. McLean <mathew.w.mclean@gmail.com>
2019-04-20 03:49:26 +1000

    Change link and hover styles

--- js/toggle.js
+++ js/toggle.js
@@ -1,32 +1,31 @@
 function toggle(divToShow) {
-	//display the selected 
-	$(".contentDiv").each(function(){
-		if(this.id == divToShow){
-			$(this).css( "display", "block" );
-		}else{
-			$(this).css( "display", "none" );
-		}
-	});	
+    //display the selected 
+    $(".contentDiv").each(function(){
+	if(this.id == divToShow){
+		$(this).css( "display", "block" );
+	}else{
+		$(this).css( "display", "none" );
+	}
+    });	
+
+    // change background of current navDiv
+    $(".navDiv a").each(function(){
+	if($.trim(this.innerHTML.toLowerCase()) == divToShow){
+	    $(this).parent().css( "background-color", "#FFFFFF" );
+	    $(this).css( "text-decoration", "underline" );
+	}else{
+	    $(this).parent().css("background-color", "#FFFFFF");
+	    $(this).css( "text-decoration", "none" );
+	}
+	// handle cv div specially
+	if ($.trim(this.innerHTML.toLowerCase()) == "html" && divToShow == "cv"){
+	    $( "div.navDiv.nav-menu" ).css( "text-decoration", "underline" );
+	}else if ($.trim(this.innerHTML.toLowerCase()) == "html"){
+	    $( "div.navDiv.nav-menu" ).css("text-decoration", "none");
+	}
+    });
 	
-	// change background of current navDiv
-	$(".navDiv a").each(function(){
-		if($.trim(this.innerHTML.toLowerCase()) == divToShow){
-			$(this).parent().css( "background-color", "#ffff80" );
-			$(this).css( "border-bottom", "#ffff80" );
-		}else{
-			$(this).parent().css("background-color", "#FFFFFF");
-			$(this).css( "border-bottom", "#FFFFFF" );
-		}
-		// handle cv div specially
-		if ($.trim(this.innerHTML.toLowerCase()) == "html" && divToShow == "cv"){
-		   
-			$( "div.navDiv.nav-menu" ).css( "background-color", "#ffff80" );
-		}else if ($.trim(this.innerHTML.toLowerCase()) == "html"){
-			$( "div.navDiv.nav-menu" ).css("background-color", "#FFFFFF");
-		}
-	});
-	
-	//display thank you to Jeff if browser window at least as big as specified size
+    //display thank you to Jeff if browser window at least as big as specified size
     var w=window.outerWidth;
     if (divToShow == "about") 
     {
